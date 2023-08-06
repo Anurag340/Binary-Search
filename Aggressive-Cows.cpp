116fc73da74df96a9ec7ff8a3f18da87fc0cadde
Anurag <115922618+Anurag340@users.noreply.github.com>
2023-08-06 21:44:54 +0530

    Aggressive-Cows.cpp

--- Aggressive-Cows.cpp
+++ Aggressive-Cows.cpp
@@ -1,50 +1,45 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std ;
 
-bool sol(int mid , vector<int>&arr , int c ){
-    int cow = 1;
-    int position = arr[0];
-    for(int i=0;i<arr.size();i++){
-        if(arr[i] - position >=mid){
-            cow++;
-            if(cow==c){
-                return true;
-            }
-            else{
-                position =arr[i];
+bool sol(int mid , vector<int>&arr , int c){
+	int cow_ct=1;
+	int pos=arr[0];
 
-            }
-        }
-    }
-    return false;
+	for(int i=1 ; i<arr.size();i++){
+		if(arr[i]-pos>=mid){
+			cow_ct++;
+			pos=arr[i];
 
+			if(cow_ct==c){
+				return true ;
+			}
+		
+			
+		}
+	}
+	return false ;
 
 }
 
+
 int main(){
-    int t;
-    cin>>t;
-    
-    while(t--){
-        int n ,c;
+	int t;
+	cin>>t;
+	while(t--){
+		 int n ,c;
 
         cin>>n>>c;
         vector<int>arr(n);
         
         
         for(int i=0; i<n;i++){
-            int x;
-            cin>>x;
-            arr.push_back(x);
+            cin>>arr[i];
         }
         sort(arr.begin(),arr.end());
 
         int s=0 ;
-        int maxi=-1;
-        for(int i =0 ; i<arr.size();i++){
-            maxi = max(maxi,arr[i]);
-        }
-        int e =maxi;
+       
+        int e =1e9;
         int ans;
         while(s<=e){
             int mid = (s+e)/2;
